2dce020e88d99079b86582ee3653ce95a1718974
passyui <132608164+passyui@users.noreply.github.com>
2025-07-05 12:43:45 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,31 @@
 const CACHE_NAME = 'anc-calculator-v1';
 const urlsToCache = [
-  'anc_calculator.html',
-  'manifest.json',
-  'icon-192.png',
-  'icon-512.png'
+  './anc_calculator.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', event => {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(name => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      )
-    )
+    caches.keys().then(names => Promise.all(
+      names.map(name => name !== CACHE_NAME ? caches.delete(name) : null)
+    ))
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(res => res || fetch(event.request))
   );
+});
+ );
 });
